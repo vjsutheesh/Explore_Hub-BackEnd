a90cfc1dd3ed13024ebf896430aa3b8ffbd3906d
vjsutheesh <vjsutheesh@gmail.com>
2023-11-19 00:35:55 +0530

    cor

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json())
 app.use('/uploads/images',express.static(path.join('uploads','images')))
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Origin','https://explore-and-live.netlify.app')
     res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization')
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE')
     next();
